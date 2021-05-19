620777557d61bb9dea098dbec7a9c7e2c5ffd130
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-05-19 02:10:37 -0300

    feat: add transactions table component

---
+++ src/components/TransactionsTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Freelance de website</td>
+          <td class="deposit">R$ 6.000,00</td>
+          <td>Dev</td>
+          <td>12/02/2021</td>
+        </tr>
+        <tr>
+          <td>Aluguel</td>
+          <td class="withdraw">R$ 4.000,00</td>
+          <td>Casa</td>
+          <td>14/02/2021</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionsTable',
+};
+</script>
+
+<style lang="stylus" scoped>
+.container {
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
+
+    th {
+      color: var(--text-body);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
+    }
+
+    td {
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--shape);
+      color: var(--text-body);
+      border-radius: 0.25rem;
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,19 +3,30 @@
     <Header />
     <main>
       <Summary />
+      <TransactionsTable />
     </main>
   </div>
 </template>
 
 <script>
-import { Header, Summary } from '@/components';
+import { Header, Summary, TransactionsTable } from '../components';
 
 export default {
   name: 'Home',
   components: {
     Header,
     Summary,
+    TransactionsTable,
   },
-  data: () => ({}),
 };
 </script>
+
+<style lang="stylus" scoped>
+.home {
+  main {
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2.5rem 1rem;
+  }
+}
+</style>
