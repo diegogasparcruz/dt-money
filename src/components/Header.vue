2b0b1327481fc4ca3d83c6b60bd8f850db284d79
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-05-19 01:24:21 -0300

    feat: create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <header>
+      <img src="@/assets/logo.svg" alt="dt money" />
+      <button type="button">Nova transação</button>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="stylus" scoped>
+.container {
+  background: var(--blue);
+
+  header {
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2rem 1rem 12rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+      font-size: 1rem;
+      color: #FFF;
+      background: var(--blue-light);
+      border: 0;
+      padding: 0 2rem;
+      border-radius: 0.25rem;
+      height: 3rem;
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.9);
+      }
+    }
+  }
+}
+</style>
